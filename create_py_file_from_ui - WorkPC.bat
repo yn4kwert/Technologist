aeaff6e3631cfc3ef795cc6edfd480aa75283065
yn4kWert <wado93@mail.ru>
2022-05-22 12:28:06 +0500

    Changed directory

--- create_py_file_from_ui - WorkPC.bat
+++ create_py_file_from_ui - WorkPC.bat
@@ -1,6 +1,6 @@
-pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\ME_calulator\main_window.ui -o C:\Users\Vmaslov6\PycharmProjects\ME_calulator\main_window.py
-pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\ME_calulator\constant_window.ui -o C:\Users\Vmaslov6\PycharmProjects\ME_calulator\constant_window.py
-pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\ME_calulator\about_window.ui -o C:\Users\Vmaslov6\PycharmProjects\ME_calulator\about_window.py
-pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\ME_calulator\pumpCalc_window.ui -o C:\Users\Vmaslov6\PycharmProjects\ME_calulator\pumpCalc_window.py
-pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\ME_calulator\new_item_window.ui -o C:\Users\Vmaslov6\PycharmProjects\ME_calulator\new_item_window.py
+pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\Technologist\main_window.ui -o C:\Users\Vmaslov6\PycharmProjects\Technologist\main_window.py
+pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\Technologist\constant_window.ui -o C:\Users\Vmaslov6\PycharmProjects\Technologist\constant_window.py
+pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\Technologist\about_window.ui -o C:\Users\Vmaslov6\PycharmProjects\Technologist\about_window.py
+pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\Technologist\pumpCalc_window.ui -o C:\Users\Vmaslov6\PycharmProjects\Technologist\pumpCalc_window.py
+pyuic5.exe -x C:\Users\Vmaslov6\PycharmProjects\Technologist\new_item_window.ui -o C:\Users\Vmaslov6\PycharmProjects\Technologist\new_item_window.py
 
